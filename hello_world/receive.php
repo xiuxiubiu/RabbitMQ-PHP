4c381684a9051082119b7147c3ed9e6b505a88ea
xiuxiubiu <lixudongk@163.com>
2018-09-23 23:34:30 +0800

    代码注释

--- hello_world/receive.php
+++ hello_world/receive.php
@@ -7,6 +7,9 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('127.0.0.1', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
+# 这里声明消费队列是正确的
+# 因为不能保证消费操作一定在发送操作后才执行
+# 所以声明消费队列保证消费时队列存在
 $channel->queue_declare('hello', false, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
