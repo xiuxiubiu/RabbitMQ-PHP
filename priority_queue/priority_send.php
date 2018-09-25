e416d545bdf6d8d4be83b6c263de15ed926764ab
xiuxiubiu <lixudongk@163.com>
2018-09-25 17:18:36 +0800

    优先级队列

--- priority_queue/priority_send.php
+++ priority_queue/priority_send.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 
-require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
